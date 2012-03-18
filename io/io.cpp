1109d184b58476d7fe94c6a8ca93a9f46790d6fd
Andrés Mejía <andmej@gmail.com>
2012-03-18 17:10:17 -0500

    Adding fast method to read unsigned integers.

--- io/io.cpp
+++ io/io.cpp
@@ -1,22 +1,30 @@
 namespace IO {
    #define MAXBUFF (1<<18)
-   char buffer[MAXBUFF], *p = buffer+MAXBUFF;
-   
-   inline char read_char() {
-      if( p == buffer+MAXBUFF ) {
-         fread( buffer, 1, MAXBUFF, stdin );
-         p = buffer;
-      }
-      return *p++;
-   }
+    char buffer[MAXBUFF], *p = buffer+MAXBUFF;
 
-   inline int read_signed_int() {
-      char c;
-      while( !isdigit(c = read_char()) and c != '-' );
-      int sign = 1;
-      if (c == '-') sign = -1, c = read_char();
-      int ret = c-'0';
-      while( isdigit(c = read_char()) ) ret = ret * 10 + c-'0';
-      return ret * sign;
-   }
+    inline char read_char() {
+        if( p == buffer+MAXBUFF ) {
+            fread( buffer, 1, MAXBUFF, stdin );
+            p = buffer;
+        }
+        return *p++;
+    }
+
+    inline int read_signed_int() {
+        char c;
+        while( !isdigit(c = read_char()) and c != '-' );
+        int sign = 1;
+        if (c == '-') sign = -1, c = read_char();
+        int ret = c-'0';
+        while( isdigit(c = read_char()) ) ret = ret * 10 + c-'0';
+        return ret * sign;
+    }
+
+    inline int read_unsigned_int() {
+        char c;
+        while( !isdigit(c = read_char()) );
+        int ret = c-'0';
+        while( isdigit(c = read_char()) ) ret = ret * 10 + c-'0';
+        return ret;
+    }
 }
